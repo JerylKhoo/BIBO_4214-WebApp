207cc4b5636e30576304b517970f03e60b289c86
acidMyke <13977709+acidMyke@users.noreply.github.com>
2023-04-18 10:28:39 +0800

    cleanup

---
+++ js/env.js
@@ -0,0 +1,2 @@
+export const BACKEND_URL = 'http://localhost:3172';
+export const TELEGRAM_BOT_LINK = 'https://t.me/JJ42TestBot?start=';
--- js/script.js
+++ js/script.js
@@ -1,107 +1,90 @@
-const backendURL = 'TOTP_SERVER_LINK' // Fill in with your TOTP Server's URL and Port
+const backendURL = 'http://localhost:3172'; // Fill in with your TOTP Server's URL and Port
 
 //TOTP
-function indexload(){
+function indexload() {
   const subcounter = setInterval(submit, 500);
   document.getElementById('totp').focus();
 }
 
-
-function check(event){
+function check(event) {
   var acode = event.which ? event.which : event.keyCode;
-  if (acode > 31 && (acode < 48 || acode > 57)){
+  if (acode > 31 && (acode < 48 || acode > 57)) {
     return false;
   }
 }
 
-
-function submit(){
+function submit() {
   var totp = document.getElementById('totp').value;
-  if (totp.length == 6){
+  if (totp.length == 6) {
     SubmitEvent(totp);
   }
 }
-
 
 function SubmitEvent(totp) {
   fetch(`${backendURL}/otp`, {
     method: 'POST',
     body: JSON.stringify({
-      token:totp
+      token: totp,
     }),
     headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
+      'Content-type': 'application/json; charset=UTF-8',
+    },
   })
-  .then(res => {
-    if (res.status < 200 || res.status >= 300)
-      throw "Error";
-    return res.json();
-  }).then(data => {
-    localStorage.setItem("key", data.key);
-    if (data.res){
-    location.href="qr.html"
-  }
-    else{
-      location.reload();
-    }
-  })
+    .then(res => {
+      if (res.status < 200 || res.status >= 300) throw 'Error';
+      return res.json();
+    })
+    .then(data => {
+      localStorage.setItem('key', data.key);
+      if (data.res) {
+        location.href = 'qr.html';
+      } else {
+        location.reload();
+      }
+    });
 }
 
-
-
-
 //QR CODE
-function qrload(){
+function qrload() {
   const getcounter = setInterval(gettotp, 15000);
 }
 
+var oldtoken = '';
 
-var oldtoken = "";
+function gettotp() {
+  const key = localStorage.getItem('key');
+  if (key == null) {
+    location.href = 'index.html';
+  }
 
-function gettotp(){
   fetch(`${backendURL}/qr`, {
     method: 'GET',
     headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
+      'x-key': key,
+      'Content-type': 'application/json; charset=UTF-8',
+    },
   })
-  .then(res => {
-    if (res.status < 200 || res.status >= 300)
-      throw "Error";
-    return res.json();
-  }).then(data => {
-    var token = data.token;
-    if (token != oldtoken){
-      GenerateQRCode(token);
-      oldtoken = token;
-    }
-  })
+    .then(res => {
+      if (res.status < 200 || res.status >= 300) {
+        location.href = 'index.html';
+      }
+      return res.json();
+    })
+    .then(data => {
+      var token = data.token;
+      if (token != oldtoken) {
+        GenerateQRCode(token);
+        oldtoken = token;
+      }
+    });
 }
 
-
-function verify(){
-  var key = localStorage.getItem("key");
-  localStorage.clear();
-  fetch(`${backendURL}/check`, {
-    method: 'POST',
-    body: JSON.stringify({
-      key
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  })
-  .then(res => {
-    if (res.status < 200 || res.status >= 300)
-      throw "Error";
-    return res.json();
-  }).then(data => {
-    if(data){
-      return
-    }
-    else{
-      location.href="index.html"
-    }
-  })
+function GenerateQRCode(encrypt) {
+  const QRElement = document.getElementById('qrcode');
+  QRElement.innerHTML = '';
+  new QRCode(QRElement, {
+    text: 'TELEGRAM_BOT_LINK' + encrypt, //Fill in with your Telegram Bot's link and paramters
+    width: 300,
+    height: 300,
+  });
 }
